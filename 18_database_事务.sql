aaeef8381e3c2e81622ef47c2bc0c2caa783dc08
zhongya666 <997934916@qq.com>
2020-11-24 00:26:14 +0800

    事务

--- 18_database_事务.sql
+++ 18_database_事务.sql
@@ -21,8 +21,48 @@ INSERT INTO account VALUES (NULL,'张三',1000),(NULL,'李四',1000);
 	张三给李四转账500元
 	1.张三账户-500
 	2.李四账户+500
-	
 */
+-- 1.开启事务 
+START TRANSACTION;
+ -- 转钱
+UPDATE account SET money=money-500 WHERE `name`='张三';
+UPDATE account SET money=money+500 WHERE `name`='李四';
+-- 回滚事务 
+ROLLBACK;
+-- 提交事务 
+COMMIT;
 
+/*222222222222222222222222222
+	事务的提交方式：手动0 自动1
+	查看事务提交方式	SELECT @@autocommit;
+	修改事务提交方式 set @@qutocommit=数字；
+*/
+SELECT @@autocommit;
+SET @@autocommit = 1;
+UPDATE account SET money=2500 WHERE `name`='张三';
+COMMIT;
 
+/*33333333333333333333333333333
+	事务的四大特征 acid 
+	1.原子性：要么成功、要么全部失败回滚
+	2.一致性：从一个一致性状态换到另一个一致性状态 
+	3.隔离性：多个并发事务之间要相互隔离 
+	4.持久性：事务一旦被提交，那么对数据库中的数据的改变就是永久性的。
+	
+	隔离级别分类：
+	1.read uncommitted 	读未提交 			脏读、不可重复读、幻读 
+	2.read commit 			读已提交			不可重复读、幻读 
+	3.repeatable read 	可重复读 			幻读 
+	4.serializable 			串行化 			无 
+	*脏读：在一个事务处理过程中读取到了另一个未提交事务中的数据 
+	*不可重复读：在一个事务处理过程中读取到了另一个事务中修改并已提交的数据 
+	*幻读：查询某个数据不存在，准备插入此记录，但执行插入时发现此记录已存在，无法插入。
+				查询某个数据不存在，准备删除此记录，却删除成功。
+	查询数据库的隔离级别 		select @@ tx_isolation 
+	修改数据库隔离级别 		set GLOBAL TRANSACTION ISOLATION LEVEL 级别字符串 
+*/
+-- 查询数据库的隔离级别(mysql8已更新)
+SELECT @@transaction_isolation;
+-- 修改数据库隔离级别(mysql8已更新)
+SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 	
